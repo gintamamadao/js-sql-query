a6191163cb312236582ea9005b7aa43fdd16056a
gintamamadao <gintamaginsan@126.com>
2019-09-07 17:16:28 +0800

    1.0-增加数据库类型连接

--- src/constant/execute/enum.ts
+++ src/constant/execute/enum.ts
@@ -1,7 +1,5 @@
 export enum DialectModules {
     mysql = "mysql",
-    mssqlPool = "tedious-connection-pool",
     mssql = "tedious",
-    postgresql = "postgresql",
-    sqlite = "sqlite"
+    mssqlPool = "tedious-connection-pool"
 }
