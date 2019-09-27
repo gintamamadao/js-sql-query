14edc1c6db71a70e49f889c9850acfc429b1de00
gintamamadao <gintamaginsan@126.com>
2019-09-28 01:22:27 +0800

    1.0-调整编译配置

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,7 +1,6 @@
 const util = require("./util");
 const typescript = require("rollup-plugin-typescript");
 const babel = require("rollup-plugin-babel");
-const nodeResolve = require("rollup-plugin-node-resolve");
 const commonjs = require("rollup-plugin-commonjs");
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
@@ -32,7 +31,6 @@ module.exports = {
     input: util.resolve("src/index.ts"),
     plugins: [
         typescript(typescriptOptions),
-        nodeResolve({ extensions }),
         commonjs({ extensions, ignore: ["conditional-runtime-dependency"] }),
         babel(babelOptions)
     ],
